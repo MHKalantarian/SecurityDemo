75b7ec365226cac00021926879fe67ed53c1fe20
MHKalantarian@gmail.com <t4455108>
2020-11-19 08:18:35 +0330

    SharedPreferenceHelper to help put and get data from shared preferences

--- app/src/main/java/com/iptsco/interview/util/Constants.java
+++ app/src/main/java/com/iptsco/interview/util/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final String AES_NOPAD_TRANS = "AES/GCM/NoPadding";
     public static final String ANDROID_KEYSTORE = "AndroidKeyStore";
     public static final String KEY_ALIAS = "IPTSCO";
+    public static final String SHARED_PREF_ALIAS = "IPTSCO";
 }
